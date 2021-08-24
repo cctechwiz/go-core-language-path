1730ac617c48434109be26bb64b08b5e9ade0c04
Josh Maxwell <jmaxwell@qualtrics.com>
2021-08-24 15:21:40 -0600

    Demo: Web Service: Basic web service implemented

--- go_the_big_picture/web_service/main.go
+++ go_the_big_picture/web_service/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!")) //Writer is expecting a slice of bytes
+	})
+
+	err := http.ListenAndServe(":3000", nil) //nil specifies to use the built-in handler
+	if err != nil {
+		log.Fatal(err)
+	}
 }
