80b4560a938ed45060084f2b34b7a21fba10c509
Josh Maxwell <jmaxwell@qualtrics.com>
2021-08-24 15:29:12 -0600

    Demo: Web Service: Query parameters and Json response

--- go_the_big_picture/web_service/main.go
+++ go_the_big_picture/web_service/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!")) //Writer is expecting a slice of bytes
+		names := r.URL.Query()["name"] //Can specify more than one name query parameter
+		var name string
+		if len(names) == 1 {
+			name = names[0]
+		}
+		m := map[string]string {"name": name}
+		enc := json.NewEncoder(w) //Anything encoded with this encoder is writen to the writer
+		enc.Encode(m)
 	})
 
 	err := http.ListenAndServe(":3000", nil) //nil specifies to use the built-in handler
