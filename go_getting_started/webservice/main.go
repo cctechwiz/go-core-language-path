7c63b345b8c05a706b2d7c6fa21208aad6a05e64
Josh Maxwell <jmaxwell@qualtrics.com>
2021-08-30 14:04:29 -0600

    DEMO: Go Getting Started: Working With Collections

--- go_getting_started/webservice/main.go
+++ go_getting_started/webservice/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/cctechwiz/go-core-language-path/go_getting_started/webservice/models"
 )
 
 func main() {
-	fmt.Println("Empty Web Service")
+	u := models.User{
+		ID: 2,
+		FirstName: "Tricia",
+		LastName: "McMillan",
+	}
+
+	fmt.Println(u)
 }
+
