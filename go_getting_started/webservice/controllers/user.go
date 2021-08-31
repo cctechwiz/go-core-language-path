ad518bb2b53de1d65851707820bb0cf235189758
Josh Maxwell <jmaxwell@qualtrics.com>
2021-08-31 15:33:14 -0600

    Demo: Go Getting Started: Adding Methods

--- go_getting_started/webservice/controllers/user.go
+++ go_getting_started/webservice/controllers/user.go
@@ -1,9 +1,20 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type userController struct{}
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
 
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the User Controller!"))
+}
+
+func newUserController() *userController {
+	return &userController{
+		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`),
+	}
 }
